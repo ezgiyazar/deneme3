a3b6e2b86814a3dc781edaf41894034c8a4248c6
ezgiyazar <ezgi.yazar@boun.edu.tr>
2022-08-16 10:10:55 +0300

    deneme3 1

--- Deneme3.java
+++ Deneme3.java
@@ -1,5 +1,11 @@
 
 
-public class Deneme3{
+
+public class Deneme3 {
     public String mythougths = "üçüncü kez commit";
+
+    public static void main(String[] args) {
+        System.out.println("olcak olcak");
+    }
+
 }
